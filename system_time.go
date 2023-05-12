5ef5cbda729ab0f2120abfe1431386df47748d58
Sune Simonsen <sune@we-knowhow.dk>
2023-05-12 08:05:50 +0200

    Added a system time file

---
+++ system_time.go
@@ -0,0 +1,24 @@
+package streams
+
+import "time"
+
+// Interface to specify the time of the system time right now.
+type SystemTime interface {
+	Now() time.Time
+}
+
+type realClock struct{}
+
+func (c realClock) Now() time.Time {
+	return time.Now()
+}
+
+// System time mock, useful for mocking the system time in tests.
+type SystemTimeMock struct {
+	Time time.Time
+}
+
+// Returns the mocked time.
+func (c *SystemTimeMock) Now() time.Time {
+	return c.Time
+}
